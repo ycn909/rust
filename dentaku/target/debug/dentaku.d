8e367ac0b2ebd1f3be24a9338d2ebfc5c5895adb
ycn909 <ycn_2003@outlook.com>
2024-08-18 10:28:49 +0900

    Delete dentaku directory

--- dentaku/target/debug/dentaku.d
+++
@@ -1,1 +0,0 @@
-C:\Users\admin\doc\rust\dentaku\target\debug\dentaku.exe: C:\Users\admin\doc\rust\dentaku\src\main.rs
--- dentaku/target/debug/deps/dentaku.d
+++
@@ -1,5 +0,0 @@
-C:\Users\admin\doc\rust\dentaku\target\debug\deps\dentaku.exe: src/main.rs
-
-C:\Users\admin\doc\rust\dentaku\target\debug\deps\dentaku.d: src/main.rs
-
-src/main.rs:
